7b7f1d52c107c53485f6176aa78bc01b815494a9
Brian Borealis & Jenna Navratil <student@epicodus.com>
2015-09-10 16:48:31 -0700

    add html and routes

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
-// var romanNumeral = function(regNumber) {
-//     var romanNum = "I";
-//     var output = romanNum.concat(romanNum);
-//     if (regNumber === 1) {
-//         return romanNum;
-//     } if (regNumber === 2) {
-//         return output;
-//     } if (regNumber === 3) {
-//         return output.concat(romanNum);
-//     }
-//
-// };
 
 var romanNumeral = function(regNumber) {
+
     var numArray = [1000, 500, 100, 50, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
     var romanArray = ["M", "D", "C", "L", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"];
     var output = [];
 
-
     while(regNumber > 0) {
-    for(var i=0; i <= numArray.length; i++) {
-        if ((regNumber >= numArray[i]) && (regNumber < 3999)) {
-            output.push(romanArray[i]);
-            regNumber -= numArray[i];
-            break;
+        for(var i=0; i <= numArray.length; i++) {
+            if ((regNumber >= numArray[i]) && (regNumber < 3999)) {
+                output.push(romanArray[i]);
+                regNumber -= numArray[i];
+                break;
+            }
         }
-
     }
-}
-    var stringOutput = output.toString();
-    var finalOutput = stringOutput.replace(",", "");
+
+    var finalOutput = output.join("");
     return finalOutput;
 
-
-
 };
+
+$(document).ready(function() {
+    $("form#romanNumeral").submit(function(event) {
+        var regNumber = parseInt($("input#regNumber").val());
+        var result = romanNumeral(regNumber);
+
+        $('.regNumber').text(regNumber);
+
+        $(".result").text(result);
+
+        $("#result").show();
+        event.preventDefault();
+    });
+});
